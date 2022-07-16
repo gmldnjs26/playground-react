679fb5100dbd0c2fe11fbb3cca71fce63ecf316b
gmldnjs26 <imleesky265@gmail.com>
2022-07-16 17:06:58 +0900

    style: add animation in CartButton

--- order/src/components/Layout/HeaderCartButton.js
+++ order/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,27 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Icons/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const cartContext = useContext(CartContext);
   const totalAmout = cartContext.items.reduce((sum, i) => sum + i.amount, 0);
+  const [isUpdatingCartItem, setIsUpdatingCartItem] = useState(false);
+  useEffect(() => {
+    setIsUpdatingCartItem(true);
+    const timer = setTimeout(() => {
+      setIsUpdatingCartItem(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+  const btnClasses = `${classes.button} ${
+    isUpdatingCartItem ? classes.bump : ""
+  }`;
 
   return (
-    <div className={classes.button} onClick={props.onClick}>
+    <div className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
