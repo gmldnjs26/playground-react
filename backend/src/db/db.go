14f16dd5fc836cfd45e06683ec56c09c5aa20dda
gmldnjs26 <imleesky265@gmail.com>
2022-08-06 01:10:50 +0900

    config: db

---
+++ backend/src/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	dsn := "root:root@tcp(db:3306)/order"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level slient 1 ~ info 4
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic("Connet Error")
+	}
+}
+
+func AutoMigrate() {
+	// enrollment models
+	DB.AutoMigrate()
+}
