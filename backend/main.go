0590969dda123255e60443f487ada9c834d9a6fe
gmldnjs26 <imleesky265@gmail.com>
2022-08-06 18:11:14 +0900

    feat: db, routes set in main.go

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"chore-api/src/db"
+	"chore-api/src/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
+	// DB
+	db.Connect()
+	db.AutoMigrate()
+
+	// Set App Routes and Cors
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+	routes.Setup(app)
 
 	app.Listen(":3030")
 }
