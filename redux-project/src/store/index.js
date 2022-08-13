345d097192dcf9eddcd106f772be8b3947882a58
gmldnjs26 <imleesky265@gmail.com>
2022-08-13 16:54:24 +0900

    feat: add authSlice

--- redux-project/src/store/index.js
+++ redux-project/src/store/index.js
@@ -1,10 +1,10 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, isLogin: false };
+const initialCounterState = { counter: 0 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state, action) {
       state.counter = state.counter + (action.payload || 1);
@@ -15,10 +15,26 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
