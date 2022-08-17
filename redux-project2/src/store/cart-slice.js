3f153fc86b298b9d7617a938010a529282e7be0f
gmldnjs26 <imleesky265@gmail.com>
2022-08-17 22:20:20 +0900

    feat: add ui-slice in redux

---
+++ redux-project2/src/store/cart-slice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialCartState = {
+  item: [],
+  totalPrice: 0,
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItem(state, action) {
+      const targetIdx = state.item.findIndex(
+        (e) => e.productId === action.payload.id
+      );
+      if (targetIdx !== -1) {
+        state.item[targetIdx].amount++;
+      } else {
+        state.item.push({
+          productId: action.payload.id,
+          title: action.payload.title,
+          description: action.payload.description,
+          price: action.payload.price,
+          amount: 1,
+        });
+      }
+    },
+    incrementAmount(state, action) {
+      const targetIndex = state.item.findIndex(
+        (e) => e.productId === action.payload
+      );
+      state.item[targetIndex].amount = state.item[targetIndex].amount + 1;
+    },
+    decrementAmount(state, action) {
+      const targetIndex = state.item.findIndex(
+        (e) => e.productId === action.payload
+      );
+      state.item[targetIndex].amount = state.item[targetIndex].amount - 1;
+    },
+  },
+});
