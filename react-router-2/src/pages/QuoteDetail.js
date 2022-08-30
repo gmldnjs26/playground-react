64d85ce070b0a324d02a391b306424be260fafdd
gmldnjs26 <imleesky265@gmail.com>
2022-08-30 22:47:49 +0900

    feat: show contents in Detail page

--- react-router-2/src/pages/QuoteDetail.js
+++ react-router-2/src/pages/QuoteDetail.js
@@ -1,12 +1,36 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_DATA = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun",
+  },
+  {
+    id: "q2",
+    author: "Min",
+    text: "Learning Typescript is fun",
+  },
+  {
+    id: "q3",
+    author: "Heewon",
+    text: "Learning Vue is fun",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_DATA.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote</p>;
+  }
   return (
     <div>
-      <h1>QuoteDetail</h1>
-      <div>{params.quoteId}</div>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path="/quotes/:quoteId/comments">
         <Comments />
       </Route>
